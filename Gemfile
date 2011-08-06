64db7dbbff1d7a38c43a3cfc37b307b5602889db
kiplrails <info@kunalinfotech.net>
2011-08-06 22:57:35 +0530

    a

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+gem 'passport'
+gem 'devise'
+gem 'nifty-generators'
+gem 'omniauth'
+gem 'mongrel', '1.2.0.pre2'       
+gem 'mysql'
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+require 'openid/store/filesystem'
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'GbaVaLd5DkFQTEX4IfKkA', 'zU2V3aLqImFWpEH0NfWwinGHopr0sPNtkfmzgZFSTiQ'
+  provider :facebook,'147998438615101', 'd4c6649e2b4392db5b1021a075c9b837'  
+  provider :open_id, OpenID::Store::Filesystem.new('/tmp') 
+
+end
