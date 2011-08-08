23701051516311759d38fb142477853b277aa7a2
kiplrails <info@kunalinfotech.net>
2011-08-08 14:44:18 +0530

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
-  def apply_omniauth(omniauth)
-    case omniauth['provider']
-    when 'facebook'
-      self.apply_facebook(omniauth)
-    end
+ def apply_omniauth(omniauth)
+ #   case omniauth['provider']
+  #  when 'facebook'
+  #    self.apply_facebook(omniauth)
+  #  end
     authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'], :token =>(omniauth['credentials']['token'] rescue nil))
   end
   
