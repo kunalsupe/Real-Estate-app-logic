e4c28e184f478610716e1c4d61109c105c9f248c
kiplrails <info@kunalinfotech.net>
2011-08-08 12:51:40 +0530

    a

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'GbaVaLd5DkFQTEX4IfKkA', 'zU2V3aLqImFWpEH0NfWwinGHopr0sPNtkfmzgZFSTiQ'
-  provider :facebook,'147998438615101', 'd4c6649e2b4392db5b1021a075c9b837',{:scope => 'publish_stream,offline_access,email'
+  provider :facebook,'147998438615101', 'd4c6649e2b4392db5b1021a075c9b837',{:scope => 'publish_stream,offline_access,email'}
   provider :linked_in,'ptze5b1nz195', 'HxGiZGKm8PEms1Nh'
   
   provider :open_id, OpenID::Store::Filesystem.new('/tmp') 
