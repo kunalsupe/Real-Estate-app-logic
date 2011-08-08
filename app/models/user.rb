f70f62710bd2405ed27e6831e791c4a22c2b0754
kiplrails <info@kunalinfotech.net>
2011-08-08 14:50:54 +0530

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,18 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
   def apply_omniauth(omniauth)
-    self.email = omniauth['user_info']['email'] if email.blank?
-    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
+    case omniauth['provider']
+    when 'facebook'
+      self.apply_facebook(omniauth)
+    end
+    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'], :token =>(omniauth['credentials']['token'] rescue nil))
   end
-        
+  
+  def facebook
+    @fb_user ||= FbGraph::User.me(self.authentications.find_by_provider('facebook').token)
+  end
+  
+   
   
   def password_required?
     (authentications.empty? || !password.blank?) && super  
@@ -28,6 +36,13 @@ class User < ActiveRecord::Base
 
       update_attributes(params) 
     end
-  
+     
+         protected
+
+    def apply_facebook(omniauth)
+      if (extra = omniauth['extra']['user_hash'] rescue false)
+        self.email = (extra['email'] rescue '')
+      end
+    end
  
 end
