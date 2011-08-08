1811f522804d2a976197bbcf8b9a2671bd7576a6
kiplrails <info@kunalinfotech.net>
2011-08-08 15:52:15 +0530

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,34 +4,24 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable 
+         :recoverable, :rememberable, :trackable
  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
-def apply_omniauth(omniauth)
-  case omniauth['provider']
+  def apply_omniauth(omniauth)
+    case omniauth['provider']
     when 'facebook'
-      self.apply_facebook(omniauth)   
-      when 'twitter'
-        self.email = omniauth['user_info']['email'] if email.blank?
-          authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
-    
-       when 'linked-in'
-         self.email = omniauth['user_info']['email'] if email.blank?
-           authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
-      
-  end   
-
-end  
+      self.apply_facebook(omniauth)
+    end
+    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
+  end 
   
   
   def facebook
     @fb_user ||= FbGraph::User.me(self.authentications.find_by_provider('facebook').token)
   end
-  
-   
-  
+
   def password_required?
     (authentications.empty? || !password.blank?) && super  
     end                                                
