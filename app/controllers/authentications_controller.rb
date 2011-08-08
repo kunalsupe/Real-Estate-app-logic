a43191576d364c1c08d04c5c9c20cda42688db1a
kiplrails <info@kunalinfotech.net>
2011-08-08 16:45:50 +0530

    a

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -10,7 +10,9 @@ class AuthenticationsController < ApplicationController
   def create
      omniauth = request.env["omniauth.auth"]
      authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
-     if authentication
+     if authentication   
+       
+      
        flash[:notice] = "Signed in successfully."
        sign_in_and_redirect(:user, authentication.user)
      elsif current_user
