cea7ceabd3a8d00e9a4171070315f6d3678e911f
kiplrails <info@kunalinfotech.net>
2011-08-08 12:12:56 +0530

    a

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -10,24 +10,24 @@ class AuthenticationsController < ApplicationController
   def create
     omniauth = request.env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid']) || User.create_with_omniauth(omniauth) 
-  #  if authentication
+    if authentication
       flash[:notice] = "Signed in successfully."
       sign_in_and_redirect(:user, authentication.user)
-  #  elsif current_user
-   #   current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
-  #    flash[:notice] = "Authentication successful."
-   #   redirect_to authentications_url
-   # else
+    elsif current_user
+      current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
+      flash[:notice] = "Authentication successful."
+      redirect_to authentications_url
+    else
       user = User.new
-    #  user.apply_omniauth(omniauth)
-     # if user.save
-      #  flash[:notice] = "Signed in successfully."
-       # sign_in_and_redirect(:user, user)
-     # else
-      #  session[:omniauth] = omniauth.except('extra')
-       # redirect_to new_user_registration_url
-     # end
-  #  end
+      user.apply_omniauth(omniauth)
+      if user.save
+        flash[:notice] = "Signed in successfully."
+        sign_in_and_redirect(:user, user)
+      else
+        session[:omniauth] = omniauth.except('extra')
+        redirect_to new_user_registration_url
+      end
+    end
   end
   
   def destroy
