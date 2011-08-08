f3fb2e618a5824c34f5fc37694c04edd1784d6f6
kiplrails <info@kunalinfotech.net>
2011-08-08 11:31:56 +0530

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,7 @@ class User < ActiveRecord::Base
     authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
   end
   
-  def email_required? 
-      false 
-    end      
+       
   
   def password_required?
     (authentications.empty? || !password.blank?) && super  
