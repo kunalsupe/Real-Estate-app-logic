ab38f63f061a421d4bdc20de14fcbddf63a76391
kiplrails <info@kunalinfotech.net>
2011-09-30 17:31:47 +0530

    user property count

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'GbaVaLd5DkFQTEX4IfKkA', 'zU2V3aLqImFWpEH0NfWwinGHopr0sPNtkfmzgZFSTiQ'
-  provider :facebook,'147998438615101', 'd4c6649e2b4392db5b1021a075c9b837',{:scope => 'publish_stream,offline_access,email'}
-  provider :linked_in,'ptze5b1nz195', 'HxGiZGKm8PEms1Nh'
+  provider :twitter, 'TUKfXMe3mf4JDUdJIuS7g', 'hzH2nOK2PWQRPolOnJUm3dJzOImjH6YhvOyfUmbN7o'
+  provider :facebook,'224567324265745', '29b03854dfc3afd54cce2af7ceed95d0',{:scope => 'publish_stream,offline_access,email'}
+  provider :linked_in,'sh1otye06apa', 'XxzzldATHdFinbwE'
   
   provider :open_id, OpenID::Store::Filesystem.new('/tmp') 
 
