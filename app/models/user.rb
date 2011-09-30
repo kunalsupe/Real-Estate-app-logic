331e4a6ac3a7b0894f66f9e11b8ec08526637f52
kiplrails <info@kunalinfotech.net>
2011-09-30 17:40:37 +0530

    user property count

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :properties
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable
  
   # Setup accessible (or protected) attributes for your model
