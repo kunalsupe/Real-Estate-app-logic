5902008bfd875f35e5a44ce1fba0c45c81727b48
kiplrails <info@kunalinfotech.net>
2011-09-30 09:02:04 +0530

    a

--- db/migrate/20110924190352_add_builder_to_properties.rb
+++
@@ -1,9 +0,0 @@
-class AddBuilderToProperties < ActiveRecord::Migration
-  def self.up
-    add_column :properties, :builder, :string
-  end
-
-  def self.down
-    remove_column :properties, :builder
-  end
-end
