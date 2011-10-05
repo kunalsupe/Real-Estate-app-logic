001992e0316d4405272d52e0c2ef1661140af661
kiplrails <info@kunalinfotech.net>
2011-10-05 09:07:17 +0530

    a

--- config/initializers/rhoconnect.rb
+++ config/initializers/rhoconnect.rb
@@ -1,5 +1,5 @@
 Rhoconnect.configure do |config|
    config.app_endpoint = 'http://housingskunaal.heroku.com'  
    config.authenticate = lambda { |credentials| 
-   User.authenticate(credentials[:login], credentials[:password]) }
+   true }
 end
